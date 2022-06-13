dd7d2b0bee6358eb18971fd0ad0628cec34ce7ae
Mark Viti <+>
2022-06-13 17:11:40 -0400

    increased search logic

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 def album_search_by_artist_name(artist_name)
-    if RSpotify::Artist.search("#{artist_name}").map { |art| art.name }.count >= 2
-        puts RSpotify::Artist.search("#{artist_name}").map { |art| art.name }.uniq
+    if RSpotify::Artist.search("#{artist_name}").map { |art| art.name }.include?(artist_name) == false
+        puts RSpotify::Artist.search("#{artist_name}").map { |art| art.name }.uniq.first(5)
 
         puts "\n"
         puts "Which #{artist_name}? If there are multiple artists with the same name, your search will return the most popular artist with that name."
@@ -12,7 +12,10 @@ def album_search_by_artist_name(artist_name)
         art_name = RSpotify::Artist.search("#{artist_name}").filter { |art| art.popularity == most_pop }[0].name
         puts RSpotify::Artist.search("#{art_name}")[0].albums.map { |alb| alb.name }.uniq
 
-    elsif RSpotify::Artist.search("#{artist_name}").map { |art| art.name }.count == 1
-        RSpotify::Artist.search("#{artist_name}")[0].albums.map { |alb| alb.name }.uniq
+    elsif RSpotify::Artist.search("#{artist_name}").map { |art| art.name }.include?(artist_name) == true
+        puts "Albums by #{artist_name}:"
+        most_pop =  RSpotify::Artist.search("#{artist_name}").map { |art| art.popularity }.max {|art1, art2| art1 <=> art2 }
+        art_name = RSpotify::Artist.search("#{artist_name}").filter { |art| art.popularity == most_pop }[0].name
+        puts RSpotify::Artist.search("#{art_name}")[0].albums.map { |alb| alb.name }.uniq
     end  
 end 
