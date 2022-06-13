df1c36d3706f23bd389a747d87766183d167e0c7
Mark Viti <+>
2022-06-13 18:24:07 -0400

    album search added artist name

--- lib/songs.rb
+++ lib/songs.rb
@@ -1,4 +1,5 @@
 def search_song(song_name)
     puts RSpotify::Track.search("#{song_name}").flat_map { |art| [art.name, art.artists[0].name, art.album.name].join(" | ") }.first(10)
 
-end 
+    
+end
