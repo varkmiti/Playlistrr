ab54e34b16148b72c8f1df85f6ec1dd2697806fd
Mark Viti <+>
2022-06-17 10:08:28 -0400

    update party

--- lib/parties.rb
+++ lib/parties.rb
@@ -10,7 +10,7 @@ end
 
 def view_songs_in_party
     puts "\n"
-    puts Song.where(playlist_id: @session_party.id).map { |song| song.name }
+    puts Song.where(party_id: @session_party.id).map { |song| song.name }.uniq
 end 
 
 def view_user_parties
@@ -18,4 +18,18 @@ def view_user_parties
 end 
 
 def update_party
+    puts "What song would you like to move to a different party?"
+    puts "\n"
+    song_name = gets.chomp
+    puts "\n"
+    puts "What party would you like to move #{song_name} from?"
+    puts "\n"
+    from_party_name = gets.chomp
+    puts "\n"
+    puts "What party would you like to move #{song_name} to?"
+    puts "\n"
+    to_party_name = gets.chomp
+    puts "\n"
+    song = Song.where(name: song_name, playlist_id: @session_playlist.id, user_id: @session_user.id, party_id: Party.where(name: from_party_name).ids[0])
+    song[0].update(party_id: Party.where(name: to_party_name).ids[0])
 end 
