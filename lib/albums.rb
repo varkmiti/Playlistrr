77b867a0f99d6239ee583b6e929950b1918f68ce
Mark Viti <+>
2022-06-17 14:37:50 -0400

    album search fix

--- lib/albums.rb
+++ lib/albums.rb
@@ -8,9 +8,7 @@ def songs_by_album(album)
         real_name = gets.chomp
         puts "\n"
         puts "Songs on #{real_name}:"
-        most_pop =  RSpotify::Album.search("#{real_name}").map { |art| art.popularity }.max {|art1, art2| art1 <=> art2 }
-        art_name = RSpotify::Album.search("#{real_name}").filter { |art| art.popularity == most_pop }[0].name
-        puts RSpotify::Album.search("#{art_name}")[0].tracks.map { |alb| alb.name }.uniq
+        puts RSpotify::Album.search("#{real_name}")[0].tracks.map { |alb| alb.name }.uniq
 
     elsif RSpotify::Album.search("#{album}").map { |art| art.name }.count == 1
         puts RSpotify::Album.search("#{album}")[0].tracks.map { |alb| alb.name }.uniq
